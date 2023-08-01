fd2843317e3a1e0034b8ce484df68ce1bf31717d
Kishan <singhkishan9911@gmail.com>
2023-08-01 15:00:34 +0530

    combine all user routes

--- Routes/User/User.js
+++ Routes/User/User.js
@@ -1,8 +1,14 @@
 const router = require('express').Router()
 const PostUser = require('./PostUser')
+const GetUser = require('./GetUser')
+const FetchUser = require('./FetchUser')
+const UpdateUser = require('./UpdateUser')
 
 // combine different routes to single route
 router.use(PostUser);
+router.use(GetUser);
+router.use(FetchUser);
+router.use(UpdateUser);
 
 // export router object 
 module.exports = router;
