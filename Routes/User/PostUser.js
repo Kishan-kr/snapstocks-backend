60bded0340c249ac9ba24750923c504ddaf6b947
Kishan <singhkishan9911@gmail.com>
2023-08-02 11:34:47 +0530

    add hashing and token generation in PostUser route

--- Routes/User/PostUser.js
+++ Routes/User/PostUser.js
@@ -1,46 +1,67 @@
 
 const User = require('../../Models/User')
-const {body, validationResult} = require('express-validator')
+const { body, validationResult } = require('express-validator')
 const router = require('express').Router()
+const bcrypt = require('bcrypt')
+const generateToken = require('../../Middlewares/GenerateToken');
 
 // Endpoint to create a user 
 router.post('/create', [
   body('name', 'Name must contain alphabets only').isAlpha()
-    .isLength({min:3}).withMessage('Name must contain minimum three letters'),
+    .isLength({ min: 3 }).withMessage('Name must contain minimum three letters'),
   body('email', 'Email is not valid').isEmail(),
-  body('password').isLength({min:8}).withMessage('Minimum length of password must be 8')
+  body('password').isLength({ min: 8 }).withMessage('Minimum length of password must be 8')
     .not().isNumeric().withMessage('Password must contain an alphabet')
     .not().isAlpha().withMessage('Password must contain a number')
+    .not().isUppercase().withMessage('Password must contain a lowercase letter')
+    .not().isLowercase().withMessage('Password must contain a uppercase letter')
 ], async (req, res) => {
   let success = false;
+
+  // destructure properties from request body 
+  const { name, email, password } = req.body;
 
   // extract errors from request 
   const errors = validationResult(req);
 
   // send errors as response if available 
-  if(!errors.isEmpty()){
+  if (!errors.isEmpty()) {
     console.log(errors.array());
     return res.status(400).json(success, errors.array());
   }
-  
-  // check if user with same email already exists
-  const userExists = await User.findOne({email: req.body.email})
 
-  if(userExists) {
-    return res.status(409).json({success, message: 'User already exists with this email'})
+  try {
+    // check if user with same email already exists
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+      return res.status(409).json({ success, message: 'User already exists with this email' })
+    }
+
+    // generate salt for the password 
+    const salt = await bcrypt.genSalt(10);
+
+    // create hash for the password 
+    const hash = await bcrypt.hash(password, salt)
+
+    // create user object from request body and send success response
+    User.create({
+      name: name,
+      email: email,
+      password: hash,
+    }).then(async (user) => {
+      // generate jwt token 
+      const token = await generateToken(user.id)
+
+      success = true;
+      res.status(200).json({ success, message: 'Successfully created account', token });
+    })
+  } catch (error) {
+    if(error) {
+      console.error(error)
+      res.status(500).json({success, error})
+    }
   }
-
-  // create hash for the password 
-
-  // create user object from request body and send success response
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  }).then((user) => {
-    success = true;
-    res.status(200).json({success, message: 'Successfully created account', user});
-  })
 })
 
 module.exports = router;
