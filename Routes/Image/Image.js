47116ca2f583ee1e575bb2c50668b3d95f5f51c6
Kishan <singhkishan9911@gmail.com>
2023-08-09 19:40:19 +0530

    combine all image routes

--- Routes/Image/Image.js
+++ Routes/Image/Image.js
@@ -1,4 +1,18 @@
 const router = require('express').Router()
+const PostImage = require('./PostImage')
+const GetUserImages = require('./GetUserImages')
+const DeleteImage = require('./DeleteImage')
+const UpdateImage = require('./UpdateImage')
+const LikeImage = require('./LikeImage')
+const DownloadImage = require('./DownloadImage')
+const ViewImage = require('./ViewImage')
 
+router.use(PostImage);
+router.use(GetUserImages);
+router.use(DeleteImage);
+router.use(UpdateImage);
+router.use(LikeImage);
+router.use(DownloadImage);
+router.use(ViewImage);
 
 module.exports = router;
